7ae9aa9f5700b44a4efda216b0cf31e3e5d245cd
Marlon Silva <josemarlondasilva48@gmail.com>
2024-03-16 03:28:46 -0300

    ajuste zip lambda

--- infra/data.tf
+++ infra/data.tf
@@ -2,6 +2,6 @@ data "aws_caller_identity" "current" {}
 
 data "archive_file" "python_lambda_package" {
   type        = "zip"
-  source_dir  = "../app"
-  output_path = "../zip/lambda_authorizer.zip"
+  source_dir  = "${path.module}/../app"
+  output_path = "${path.module}/zip/lambda_authorizer.zip"
 }
