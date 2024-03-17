a0446bc7ed3d2d1424c9a3e353cab8f13107dc4e
Marlon Silva <josemarlondasilva48@gmail.com>
2024-03-17 17:09:23 -0300

    ajuste

--- infra/cognito.tf
+++ infra/cognito.tf
@@ -2,10 +2,10 @@ resource "aws_cognito_user_pool" "my_user_pool" {
   name = "blues-burger-user-pool"
 
   lambda_config {
-    define_auth_challenge = local.lambda_name_define_arn
-    create_auth_challenge = local.lambda_name_create_arn
-    verify_auth_challenge = local.lambda_name_verify_arn
-    pre_sign_up           = local.lambda_name_signup_arn
+    define_auth_challenge          = local.lambda_name_define_arn
+    create_auth_challenge          = local.lambda_name_create_arn
+    verify_auth_challenge_response = local.lambda_name_verify_arn
+    pre_sign_up                    = local.lambda_name_signup_arn
   }
 
   schema {
